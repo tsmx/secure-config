83fbf03fb8164580eb2164a523099c27b4d41062
tsmx <strs@freenet.de>
2021-09-17 20:11:13 +0200

    Jest updated

--- test/secure-config.test.js
+++ test/secure-config.test.js
@@ -6,7 +6,7 @@ describe('secure-config test suite', () => {
         delete process.env['NODE_ENV'];
     });
 
-    it('tests a successful production configuration retrival', async (done) => {
+    it('tests a successful production configuration retrival', async () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'production';
         const conf = require('../secure-config');
@@ -16,10 +16,9 @@ describe('secure-config test suite', () => {
         expect(conf.filestorage.type).toBe('local');
         expect(conf.filestorage.params.folder).toBe('/tmp/storage');
         expect(conf.filestorage.params.storagepass).toBe('StoragePassword-Prod');
-        done();
     });
 
-    it('tests a successful configuration retrival with a hexadecimal key', async (done) => {
+    it('tests a successful configuration retrival with a hexadecimal key', async () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '9af7d400be4705147dc724db25bfd2513aa11d6013d7bf7bdb2bfe050593bd0f';
         process.env['NODE_ENV'] = 'hex';
         const conf = require('../secure-config');
@@ -29,10 +28,9 @@ describe('secure-config test suite', () => {
         expect(conf.filestorage.type).toBe('local');
         expect(conf.filestorage.params.folder).toBe('/tmp/storage');
         expect(conf.filestorage.params.storagepass).toBe('StoragePassword-Hex');
-        done();
     });
 
-    it('tests a successful development configuration retrival', async (done) => {
+    it('tests a successful development configuration retrival', async () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = '';
         const conf = require('../secure-config');
@@ -46,10 +44,9 @@ describe('secure-config test suite', () => {
         expect(Array.isArray(conf.testarray)).toBeTruthy();
         expect(conf.testarray.length).toBe(3);
         expect(conf.nullvalue).toBe(null);
-        done();
     });
 
-    it('tests a successful configuration retrival without any encryption', async (done) => {
+    it('tests a successful configuration retrival without any encryption', async () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'unencrypted';
         const conf = require('../secure-config');
@@ -60,33 +57,28 @@ describe('secure-config test suite', () => {
         expect(conf.filestorage.type).toBe('local');
         expect(conf.filestorage.params.folder).toBe('/tmp/storage');
         expect(conf.filestorage.params.storagepass).toBe('storagepass');
-        done();
     });
 
-    it('tests a failed configuration retrival because of a missing encryption key', async (done) => {
+    it('tests a failed configuration retrival because of a missing encryption key', async () => {
         process.env['NODE_ENV'] = '';
         expect(() => { const conf = require('../secure-config'); }).toThrow('Environment variable CONFIG_ENCRYPTION_KEY not set.');
-        done();
     });
 
-    it('tests a failed configuration retrival because of an encryption key which length is not 32 bytes', async (done) => {
+    it('tests a failed configuration retrival because of an encryption key which length is not 32 bytes', async () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjkly';
         process.env['NODE_ENV'] = '';
         expect(() => { const conf = require('../secure-config'); }).toThrow('CONFIG_ENCRYPTION_KEY length must be 32 bytes.');
-        done();
     });
 
-    it('tests a failed configuration retrival because of a not existing configuration file', async (done) => {
+    it('tests a failed configuration retrival because of a not existing configuration file', async () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'UNKNOWN';
         expect(() => { const conf = require('../secure-config'); }).toThrow('Configuration file for NODE_ENV UNKNOWN does not exist.');
-        done();
     });
 
-    it('tests a failed configuration retrival because of a wrong key', async (done) => {
+    it('tests a failed configuration retrival because of a wrong key', async () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklXXX';
         process.env['NODE_ENV'] = 'production';
         expect(() => { const conf = require('../secure-config'); }).toThrow();
-        done();
     });
 });
