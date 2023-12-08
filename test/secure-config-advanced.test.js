0ce2fd601b2c293959f0b3a3e208b1ff5064e79e
tsmx <dev@tsmx.net>
2023-12-08 22:13:47 +0100

    typos fixed

--- test/secure-config-advanced.test.js
+++ test/secure-config-advanced.test.js
@@ -7,7 +7,7 @@ describe('secure-config adcanced features test suite (v2 features)', () => {
         delete process.env['NODE_ENV'];
     });
 
-    it('tests a successful production configuration retrival with custom key variable name', () => {
+    it('tests a successful production configuration retrieval with custom key variable name', () => {
         process.env['CUSTOM_CONFIG_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'production';
         const conf = require('../secure-config')({ keyVariable: 'CUSTOM_CONFIG_KEY' });
@@ -19,7 +19,7 @@ describe('secure-config adcanced features test suite (v2 features)', () => {
         expect(conf.filestorage.params.storagepass).toBe('StoragePassword-Prod');
     });
 
-    it('tests a successful production configuration retrival with HMAC validation', () => {
+    it('tests a successful production configuration retrieval with HMAC validation', () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'production';
         const conf = require('../secure-config')({ hmacValidation: true });
@@ -31,7 +31,7 @@ describe('secure-config adcanced features test suite (v2 features)', () => {
         expect(conf.filestorage.params.storagepass).toBe('StoragePassword-Prod');
     });
 
-    it('tests a successful production configuration retrival with HMAC validation and a custom property name', () => {
+    it('tests a successful production configuration retrieval with HMAC validation and a custom property name', () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'production-hmacproperty';
         const conf = require('../secure-config')({ hmacValidation: true, hmacProperty: '_signature' });
@@ -43,13 +43,13 @@ describe('secure-config adcanced features test suite (v2 features)', () => {
         expect(conf.filestorage.params.storagepass).toBe('StoragePassword-Prod');
     });
 
-    it('tests a failed production configuration retrival because of a failed HMAC validation (HMAC manipulated)', () => {
+    it('tests a failed production configuration retrieval because of a failed HMAC validation (HMAC manipulated)', () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'production-hmacerror';
         expect(() => { require('../secure-config')({ hmacValidation: true }); }).toThrow('HMAC validation failed.');
     });
 
-    it('tests a failed production configuration retrival because of a failed HMAC validation (configuration value manipulated)', () => {
+    it('tests a failed production configuration retrieval because of a failed HMAC validation (configuration value manipulated)', () => {
         process.env['CONFIG_ENCRYPTION_KEY'] = '0123456789qwertzuiopasdfghjklyxc';
         process.env['NODE_ENV'] = 'production-hmacerror2';
         expect(() => { require('../secure-config')({ hmacValidation: true }); }).toThrow('HMAC validation failed.');
